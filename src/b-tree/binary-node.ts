d8ced3b907e97a8948fe2a753fc5695ada864ef6
Mark Mayer <mdm373@gmail.com>
2020-05-15 10:50:16 -0400

    clean up

--- src/b-tree/binary-node.ts
+++ src/b-tree/binary-node.ts
@@ -1,5 +1,4 @@
 export type BinaryNode<T> = Readonly<{
-    parent?: BinaryNode<T>
     left?: BinaryNode<T>
     right?: BinaryNode<T>
     value: T
--- src/b-tree/compare.ts
+++ src/b-tree/compare.ts
@@ -1,11 +1,4 @@
 export type CompareValue<T> = (one: T, two: T) => number
 
-export const defaultCompare: CompareValue<any> = (one, two) => {
-  if (one === two) {
-    return 0
-  }
-  if (one > two) {
-    return 1
-  }
-  return -1
-}
+export const defaultCompare: CompareValue<any> = (one, two) => one - two
+
